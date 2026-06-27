cad13800bda0e477dcc91696f850c20e432c1949
“Emmanuele <emmanuele.online@gmail.com>
2026-06-27 18:48:24 +0200

    fix: update site logo and favicon assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,20 +27,20 @@ export const metadata: Metadata = {
   robots: "index, follow",
   icons: {
     icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" }
+      { url: "/images/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/images/favicon-32x32.png", sizes: "32x32", type: "image/png" }
     ],
     apple: [
-      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }
+      { url: "/images/apple-touch-icon.png", sizes: "180x180", type: "image/png" }
     ],
     other: [
       {
-        url: "/android-chrome-192x192.png",
+        url: "/images/android-chrome-192x192.png",
         sizes: "192x192",
         type: "image/png"
       },
       {
-        url: "/android-chrome-512x512.png",
+        url: "/images/android-chrome-512x512.png",
         sizes: "512x512",
         type: "image/png"
       }
